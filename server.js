2805659064668c8e2d94f4355707cbe5721f50af
Vijay Das <vijaytdas@gmail.com>
2019-05-31 11:07:16 -0500

    add routers folder and placeholder files

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+// import routers
+
+const actionRouter = require("./routers/actionRouter");
+const projectRouter = require("./routers/projectRouter");
+
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
 server.use(logger);
+
+// server.use the routers
+
+server.use("/actions", actionRouter);
+server.use("/projects", projectRouter);
+
+// custom logger middleware
 
 function logger(req, res, next) {
   console.log(`A ${req.method} request to '${req.url}' at '${Date.now()}`);
