fec316ccafe1e47c951061b46a994a919ca49490
Vijay Das <vijaytdas@gmail.com>
2019-06-01 08:35:21 -0500

    add basic client displays project list

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import axios from 'axios';
+//import { Route, NavLink } from "react-router-dom";
+
+import './App.css';
+import ProjectList from './components/ProjectList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Project List</h1>
+      < ProjectList />
+    </div>
+  );
+}
+
+export default App;
